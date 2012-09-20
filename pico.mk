a2e86a07762e7c96844b9578eb556ddbbe5f0574
sakindia123 <sa6915@gmail.com>
2012-09-20 17:39:40 +0530

    de-derpify sound

--- pico.mk
+++ pico.mk
@@ -35,7 +35,7 @@ PRODUCT_PACKAGES += \
 # Audio
 PRODUCT_PACKAGES += \
     audio.primary.msm7x27a \
-    audio_policy.msm7x27a\
+    audio_policy.msm7x27a \
     audio.a2dp.default \
     libaudioutils
 
